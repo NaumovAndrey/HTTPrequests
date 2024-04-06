05b870ef0058615b0eaa3ef552f05f34d58ce3ee
andrey <naumov.a.n@icloud.com>
2024-04-06 22:01:09 +0700

    Добавьте файлы проекта.

---
+++ HTTPrequests.cpp
@@ -0,0 +1,12 @@
+﻿// HTTPrequests.cpp: определяет точку входа для приложения.
+//
+
+#include "HTTPrequests.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
